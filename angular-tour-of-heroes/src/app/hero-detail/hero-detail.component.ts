c93a437913d28ddbd10290c04344fd13af55e58d
Mike <miversen33@gmail.com>
2019-05-18 23:13:16 -0500

    [Not Complete] Stage 7
    Left off at https://angular.io/tutorial/toh-pt6#delete-a-hero

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -35,4 +35,18 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
 }
